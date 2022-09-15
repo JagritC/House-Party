7741ec4c1d8501fea00d4dee10af62e1fbb3f24e
Jagrit Chaitanya <jchaitanya@hotmail.com>
2022-09-14 21:24:27 -0400

    Components and POST request
    at this point the front end has some content, we also POST the info from the frontend to the backend

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -12,8 +12,44 @@ import { Link } from "react-router-dom";
 
 export default class CreateRoomPage extends Component {
 	defaultVotes = 2;
+
 	constructor(props) {
 		super(props);
+		//whenever the state changes the component is updated
+		this.state = {
+			guestCanPause: true,
+			votesToSkip: this.votesToSkip,
+		};
+		this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
+		this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+		this.handleVotesChange = this.handleVotesChange.bind(this);
+	}
+
+	handleVotesChange(e) {
+		this.setState({
+			votesToSkip: e.target.value,
+		});
+	}
+
+	handleGuestCanPauseChange(e) {
+		this.setState({
+			guestCanPause: e.target.value === "true" ? true : false,
+		});
+	}
+	//communicating with the backend
+	handleRoomButtonPressed() {
+		const requestOptions = {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				//the variable names need to match with what I'm looking for in the backend in views.py
+				votes_to_skip: this.state.votesToSkip,
+				guest_can_pause: this.state.guestCanPause,
+			}),
+		};
+		fetch("/api/create-room", requestOptions)
+			.then((response) => response.json())
+			.then((data) => console.log(data));
 	}
 
 	render() {
@@ -27,31 +63,11 @@ export default class CreateRoomPage extends Component {
 				<Grid item xs={12} align="center">
 					<FormControl component="fieldset">
 						<FormHelperText>
-							<div align="center">
-								Guest Control of
-								Playback State
-							</div>
+							<div align="center">Guest Control of Playback State</div>
 						</FormHelperText>
-						<RadioGroup
-							row
-							defaultValue="true"
-						>
-							<FormControlLabel
-								value="true"
-								control={
-									<Radio color="primary" />
-								}
-								label="Play/Pause"
-								labelPlacement="bottom"
-							/>
-							<FormControlLabel
-								value="false"
-								control={
-									<Radio color="secondary" />
-								}
-								label="No Control"
-								labelPlacement="bottom"
-							/>
+						<RadioGroup row defaultValue="true" onChange={this.handleGuestCanPauseChange}>
+							<FormControlLabel value="true" control={<Radio color="primary" />} label="Play/Pause" labelPlacement="bottom" />
+							<FormControlLabel value="false" control={<Radio color="secondary" />} label="No Control" labelPlacement="bottom" />
 						</RadioGroup>
 					</FormControl>
 				</Grid>
@@ -60,21 +76,27 @@ export default class CreateRoomPage extends Component {
 						<TextField
 							required={true}
 							type="number"
-							defaultValue={
-								this
-									.defaultVotes
-							}
+							onChange={this.handleVotesChange}
+							defaultValue={this.defaultVotes}
 							inputProps={{
+								style: { textAlign: "center" },
 								min: 1,
 							}}
 						/>
 						<FormHelperText>
-							<div align="center">
-								Votes required
-								to skip song
-							</div>
+							<div align="center">Votes required to skip song</div>
 						</FormHelperText>
 					</FormControl>
+				</Grid>
+				<Grid item xs={12} align="center">
+					<Button color="secondary" variant="contained" onClick={this.handleRoomButtonPressed}>
+						Create a Room
+					</Button>
+				</Grid>
+				<Grid item xs={12} align="center">
+					<Button color="primary" variant="contained" to="/" component={Link}>
+						Back
+					</Button>
 				</Grid>
 			</Grid>
 		);
