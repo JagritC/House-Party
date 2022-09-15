59e233126b26f8a76764a72ed67436bb28ede9e7
Jagrit Chaitanya <jchaitanya@hotmail.com>
2022-09-14 20:36:26 -0400

    added some things

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -10,41 +10,73 @@ import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import { Link } from "react-router-dom";
 
-
-
-
 export default class CreateRoomPage extends Component {
-  defaultVotes = 2;
-  constructor(props) {
-    super(props);
-  }
+	defaultVotes = 2;
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return <Grid container spacing = {1}>
-      <Grid item xs={12} align = "center">
-        <Typography component="h4" variant = "h4">
-          Create a Room
-        </Typography>
-      </Grid>
-      <Grid item xs={12} align = "center">
-        <FormControl component="fieldset">
-          <FormHelperText>
-            <div align = "center">
-              Guest Control of Playback State
-            </div>
-          </FormHelperText>
-          <RadioGroup row defaultValue="true">
-            <FormControlLabel value = "true" control = {<Radio color = "primary" />} label = "Play/Pause" labelPlacement="bottom"/>
-            <FormControlLabel value = "false" control = {<Radio color = "secondary" />} label = "No Control" labelPlacement="bottom"/>
-          </RadioGroup>
-        </FormControl>
-      </Grid>
-      <Grid item xs={12} align = "center">
-        <FormControl>
-          <TextField required ={true} type = 'number' defaultValue={this.defaultVotes} inputProps={{min:1,}}></TextField>
-        </FormControl>
-      </Grid>
-
-    </Grid>
-  }
+	render() {
+		return (
+			<Grid container spacing={1}>
+				<Grid item xs={12} align="center">
+					<Typography component="h4" variant="h4">
+						Create a Room
+					</Typography>
+				</Grid>
+				<Grid item xs={12} align="center">
+					<FormControl component="fieldset">
+						<FormHelperText>
+							<div align="center">
+								Guest Control of
+								Playback State
+							</div>
+						</FormHelperText>
+						<RadioGroup
+							row
+							defaultValue="true"
+						>
+							<FormControlLabel
+								value="true"
+								control={
+									<Radio color="primary" />
+								}
+								label="Play/Pause"
+								labelPlacement="bottom"
+							/>
+							<FormControlLabel
+								value="false"
+								control={
+									<Radio color="secondary" />
+								}
+								label="No Control"
+								labelPlacement="bottom"
+							/>
+						</RadioGroup>
+					</FormControl>
+				</Grid>
+				<Grid item xs={12} align="center">
+					<FormControl>
+						<TextField
+							required={true}
+							type="number"
+							defaultValue={
+								this
+									.defaultVotes
+							}
+							inputProps={{
+								min: 1,
+							}}
+						/>
+						<FormHelperText>
+							<div align="center">
+								Votes required
+								to skip song
+							</div>
+						</FormHelperText>
+					</FormControl>
+				</Grid>
+			</Grid>
+		);
+	}
 }
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,29 +2,35 @@ import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	Redirect,
 } from "react-router-dom";
 
 export default class HomePage extends Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <p>This is the home page</p>
-          </Route>
-          <Route path="/join" component={RoomJoinPage} />
-          <Route path="/create" component={CreateRoomPage} />
-        </Switch>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<Switch>
+					<Route exact path="/">
+						<p>This is the home page</p>
+					</Route>
+					<Route
+						path="/join"
+						component={RoomJoinPage}
+					/>
+					<Route
+						path="/create"
+						component={CreateRoomPage}
+					/>
+				</Switch>
+			</Router>
+		);
+	}
 }
